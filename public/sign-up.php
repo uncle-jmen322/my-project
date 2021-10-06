df00863c71152b148f0f1b284955d161e984c230
Yevhenii Oksom <just.onechance322@gmail.com>
2021-10-06 18:29:27 +0300

    Add login page

---
+++ public/sign-up.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/sign-up-style.css">
+  <title>Login</title>
+</head>
+<body>
+
+
+<div class="sign-up-form-wrapper">
+
+  <form class="login-form" action="" method="post">
+  <fieldset>
+    <legend>SIGN IN</legend>
+    <p><strong>Login:</strong>
+      <input maxlength="25" size="40" name="login">
+    </p>
+    <p><strong>Password:</strong> 
+      <input type="password" maxlength="25" size="40" name="password">
+    </p>
+  <p><input class="sign-in-button" type="submit" value="SIGN IN"></p>
+  </fieldset>
+</form>
+
+<form class="register-form" action="" method="post">
+  <fieldset>
+    <legend>REGISTER</legend>
+      <p><strong>Login:</strong>
+        <input maxlength="25" size="40" name="login">
+    </p>
+    <p><strong>Password:</strong> 
+      <input type="password" maxlength="25" size="40" name="password">
+    </p>
+    <p><strong>Confirm Password:</strong> 
+      <input type="password" maxlength="25" size="40" name="password">
+    </p>
+    <p><input class="register-button" type="submit" value="CREATE ACCOUNT"></p>
+  </fieldset>
+</form>
+
+</div>
+
+
+
+
+
+	
+</body>
+</html>
+
+
+<?php
+
+
+?>
